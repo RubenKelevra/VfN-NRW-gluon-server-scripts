7fef1863bf6cc32f144f96df70ec9b6bd8d06058
Ruben Kelevra <cyrond@gmail.com>
2015-04-18 20:03:33 +0200

    add fq as qdisc

--- basic-config/etc/openvpn/tun-01_up.sh
+++ basic-config/etc/openvpn/tun-01_up.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-ip route replace 0.0.0.0/1 via $5 table 42
-ip route replace 128.0.0.0/1 via $5 table 42
+ip route replace 0.0.0.0/1 via $4 table 42
+ip route replace 128.0.0.0/1 via $4 table 42
+ifconfig tun-vpn-01 txqueuelen 10000
+tc qdisc replace dev tun-vpn-01 root fq limit 10000 flow_limit 50 buckets 256 quantum 328 initial_quantum 15140
+#tc qdisc replace dev tun-vpn-01 root fq_codel limit 10000 quantum 500 noecn
 exit 0
 
